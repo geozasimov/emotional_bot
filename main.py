c2fbdeb7db1752b468ca9e549aa758d892875d59
Georgii Zasimov <52992915+geozasimov@users.noreply.github.com>
2025-01-20 21:20:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
-import logging
+import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import BotCommand
 from aiogram.fsm.storage.memory import MemoryStorage
-from config import TOKEN
-from handlers.commands import register_handlers
-from db.database import init_db
+from aiogram.types import BotCommand
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
+from handlers.recommendations import ButtomRouter
+from handlers import *
+from handlers.gigachat.gigachat_mental import quiz_router
+from utils import config
+from database import repository
 
-# Инициализация бота и диспетчера
-bot = Bot(token=TOKEN, parse_mode="HTML")
-dp = Dispatcher(storage=MemoryStorage())
-
-# Регистрация команд
-async def set_bot_commands():
+async def set_commands(bot: Bot):
     commands = [
-        BotCommand(command="start", description="Начать работу с ботом"),
-        BotCommand(command="motivate", description="Получить мотивацию"),
-        BotCommand(command="emotion", description="Узнать описание эмоции"),
-        BotCommand(command="add_emotion", description="Добавить новую эмоцию"),
+        BotCommand(command="/start", description="Начать работу с ботом"),
+        BotCommand(command="/registration", description="Пройти регистрацию"),
+        BotCommand(command="/edit", description="Подкорректируйте данные"),
+        BotCommand(command="/profile", description="Ваши данные"),
+        BotCommand(command="/report", description="Отчет о вашем состоянии")
     ]
     await bot.set_my_commands(commands)
 
-# Инициализация
 async def main():
-    logging.info("Запуск бота...")
-    await init_db()  # Создание базы данных, если её нет
-    register_handlers(dp)  # Регистрация обработчиков
-    await set_bot_commands()  # Установка команд
-    await dp.start_polling(bot)
+    cfg = config.load()
+
+    telegram_bot = Bot(token=cfg.telegram_token)
+
+    dispatcher = Dispatcher(storage=MemoryStorage())
+    scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
+
+    dispatcher.include_router(router)
+    dispatcher.include_router(quiz_router)
+    dispatcher.include_router(ButtomRouter)
+    dispatcher.include_router(gigachat_router)
+
+    dispatcher.update.middleware(UserAuthorizationMiddleware()) 
+    dispatcher.update.middleware(UserActionLoggerMiddleware())
+    dispatcher.update.middleware(SchedulerMiddleware(scheduler=scheduler))
+
+    await repository.db.connect()
+
+    await set_commands(telegram_bot) 
+
+    print("Бот запущен. Ожидаем сообщений...")
+    scheduler.start()
+
+    await dispatcher.start_polling(telegram_bot)
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
