132be378faf8b3bba6b112d20981c04524d9ea70
Georgii Zasimov <52992915+geozasimov@users.noreply.github.com>
2025-01-24 13:58:34 +0300

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,15 @@
+from .handler import *
+from .daily_survey import *
+from .profile import *
+from .recommendations import *
+from .registration import *
+from .start import *
+from .water_reminders import *
+from .report import *
+
+from .gigachat.gigachat_handler import *
+from .gigachat.gigachat_recomendations import *
+
+from .middlewares.action_logger import *
+from .middlewares.authorization import *
+from .middlewares.scheduler import *
