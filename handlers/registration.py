129735f53dcbd8dfbec68250d8e30ee763dbb601
Ruslan Chibirov <chibirovruslan@gmail.com>
2025-01-26 18:19:43 +0300

    Update registration.py

--- handlers/registration.py
+++ handlers/registration.py
@@ -3,9 +3,7 @@ from database import repository
 from handlers import daily_survey
 from handlers import review
 
-from aiogram import Router
 from aiogram import Bot
-from aiogram import types
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.filters import Command
 from aiogram.types import Message
@@ -17,20 +15,11 @@ from zoneinfo import ZoneInfo
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
-router = Router()
-
 class Registration(StatesGroup):
     name = State()
     age = State()
     gender = State()
-
-recommendation_keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text="Рекомендации по эмоциональному состоянию", callback_data="emotion_recommendations")],
-        
-    ]
-)
-
+    
 @router.message(Command('registration'))
 async def start_registration(message: Message, state: FSMContext):
     user = await repository.get_user(message.from_user.id)
@@ -42,6 +31,13 @@ async def start_registration(message: Message, state: FSMContext):
 
 @router.message(Registration.name)
 async def add_name(message: Message, state: FSMContext):
+    if not message.text.isalpha():
+        await message.answer("Пожалуйста, введите имя буквами.")
+        return
+    if  len(message.text) > 20:
+        await message.answer("Имя слишком длинное.")
+        return
+
     await state.update_data(name=message.text)
     await state.set_state(Registration.age)
     await message.answer('Введите ваш возраст:')
@@ -56,25 +52,22 @@ async def add_age(message: Message, state: FSMContext):
     await message.answer('Введите ваш пол(м/ж):')
 
 @router.message(Registration.gender)
-async def add_gender(message: types.Message, state: FSMContext):
-    await state.update_data(gender=message.text)
-    data = await state.get_data()
-    await message.answer(f"Вы указали пол: {data['gender']}")
-    
-@router.message(Registration.gender)
-async def add_gender(message: types.Message, state: FSMContext):
+async def add_gender(message: Message, state: FSMContext):
     if message.text.lower() != "м" and message.text.lower() != "ж":
         await message.answer("Пожалуйста, введите свой пол корректно(м/ж)")
         return
+    await state.update_data(gender=message.text)
+    await callback.message.edit_reply_markup(reply_markup=None)
+
     data = await state.get_data()
-    await message.answer(f"Вы указали пол: {data['gender']}")
+    await state.clear()
+
     await repository.add_user(
-        user_id = message.from_user.id,
+        user_id=callback.from_user.id,
         name=data.get("name"),
         age=int(data.get("age")),
-        gender=data.get("gender")
+        gender=data.get("gender"),
     )
 
-    await callback.message.answer(f"Приятно познакомиться, {data.get('name')}!\nТеперь каждый день вечером вам будет предложено пройти анкетирование для отслеживание вашего состояния.\nКаждую неделю вы сможете смотреть отчет и получать мою рецензию!")
-   
+    await callback.message.answer(f"Приятно познакомиться, {data.get('name')}!")
 
