1698217ae05c7420d6413f4ce5c704819cde4eb5
Ruslan Chibirov <chibirovruslan@gmail.com>
2025-01-26 17:38:35 +0300

    Update authorization.py

--- handlers/middlewares/authorization.py
+++ handlers/middlewares/authorization.py
@@ -12,21 +12,28 @@ class UserAuthorizationMiddleware(BaseMiddleware):
         data: Dict[str, Any]
     ) -> Any:
         
-        state = data.get('raw_state')
+        state = data.get('raw_state', None)
 
-        if state in [Registration.name.state, Registration.age.state, Registration.gender.state]:
+        if state and state.state in [Registration.name.state, Registration.age.state, Registration.gender.state]:
             return await handler(event, data)
 
-        user_id = 0
+        user_id = None
 
-        if event.message:  
+        if event.message:
             user_id = event.message.from_user.id
-        elif event.callback_query:  
+        elif event.callback_query:
             user_id = event.callback_query.from_user.id
+
+        if not user_id:
+            return await handler(event, data)
         
         user = await repository.get_user(user_id)
-        if not user and not event.message.text.startswith('/registration') and not event.message.text.startswith('/start'):
-            await event.message.answer("Вы не зарегистрированы. Пожалуйста, зарегистрируйтесь, чтобы использовать бота.")
-            return
+        
+        if not user:
+            if event.message and event.message.text:
+                if not event.message.text.startswith('/registration') and not event.message.text.startswith('/start'):
+                    await event.message.answer("Вы не зарегистрированы. Пожалуйста, зарегистрируйтесь, чтобы использовать бота.")
+                    return
     
         return await handler(event, data)
+
